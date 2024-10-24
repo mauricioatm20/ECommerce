b8cb1275e352016588f5c03c6237f0ebb921155c
mauricio <mauriciochasi@gmail.com>
2024-10-25 00:38:38 +0200

    properties and clase principal

--- src/main/java/com/code/ecom/EcomApplication.java
+++ src/main/java/com/code/ecom/EcomApplication.java
@@ -2,6 +2,10 @@ package com.code.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcomApplication {
@@ -10,4 +14,17 @@ public class EcomApplication {
 		SpringApplication.run(EcomApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
