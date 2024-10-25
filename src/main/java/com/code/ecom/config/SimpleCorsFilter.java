ecd857a2c318a6177107d870838ef73c75491d90
mauricio <mauriciochasi@gmail.com>
2024-10-25 19:01:32 +0200

    setheader

--- src/main/java/com/code/ecom/config/SimpleCorsFilter.java
+++ src/main/java/com/code/ecom/config/SimpleCorsFilter.java
@@ -34,7 +34,8 @@ public class SimpleCorsFilter implements Filter {
         String originHeader = request.getHeader("origin");
 
         // Configura los headers de CORS, usando la cabecera 'Origin' capturada
-        response.setHeader("Access-Control-Allow-Origin", originHeader);
+       // response.setHeader("Access-Control-Allow-Origin", originHeader);
+        response.setHeader("Access-Control-Allow-Origin", clientAppUrl);
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT ");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
