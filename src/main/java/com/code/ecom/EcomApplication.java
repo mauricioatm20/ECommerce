0d2fc7c588cc5fd82ff2d7c23317aa0b10db3958
mauricio <mauriciochasi@gmail.com>
2024-10-25 15:04:29 +0200

    proper

--- src/main/java/com/code/ecom/EcomApplication.java
+++ src/main/java/com/code/ecom/EcomApplication.java
@@ -2,10 +2,7 @@ package com.code.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class EcomApplication {
@@ -14,20 +11,5 @@ public class EcomApplication {
 		SpringApplication.run(EcomApplication.class, args);
 	}
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedOrigins("https://ecom-web-mauricio.vercel.app") // dominio del frontend en Vercel
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-							.allowedHeaders("*") // permite todos los encabezados
-							.allowCredentials(true); // permite cookies si se necesita autenticación de sesión
-				}
-			};
-		}
-	}
+
 }
